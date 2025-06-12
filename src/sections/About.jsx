f90cd7f06edafc020beb29e9e6350a511647056c
GabrielPivoto <gabriel-pivoto@outlook.com>
2025-06-12 09:38:46 -0300

    About Me page minor fixes

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
         <section className="c-space section-spacing">
             <h2 className="text-heading">About me</h2>
             <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem] mt-12">
-                <div className="flex items-end grid-default-color grid-1">
+                <div className="flex items-end grid-black-color grid-1">
                     <img
                         src="assets/coding-pov.png"
                         className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
@@ -26,7 +26,7 @@ const About = () => {
                     </div>
                     <div className="absolute inset-x-0 pointer-events-none -bottom-4 -h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
                 </div>
-                <div className="grid-black-color grid-2">
+                <div className="grid-default-color grid-2">
                     <div ref={grid2Container} className="flex items-center justify-center w-full h-full">
                         <p className="flex items-end text-5xl text-gray-500">CODE IS CRAFT</p>
                         <Card style={{ rotate: "75deg", top: "30%", left: "5%" }} text="SOLID" containerRef={grid2Container} />
